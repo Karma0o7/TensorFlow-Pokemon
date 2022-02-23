228505aaa04482587eab6ab695bb2ec9c5b26a39
Karma0o7 <23173amitkanand@gmail.com>
2022-02-23 17:17:17 +0530

    Added function for processing data

--- tensorFlow.py
+++ tensorFlow.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -6,7 +7,6 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 df = pd.read_csv(".\pokemon.csv")
-df.columns
 df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
 df['isLegendary'] = df['isLegendary'].astype(int)
 
@@ -18,3 +18,39 @@ def dummy_creation(df, dummy_categories):
     return df
 
 df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
+
+def train_test_splitter(DataFrame, column):
+    df_train = DataFrame.loc[df[column] != 1]
+    df_test = DataFrame.loc[df[column] == 1]
+
+    df_train = df_train.drop(column, axis=1)
+    df_test = df_test.drop(column, axis=1)
+
+    return(df_train,df_test)
+
+df_train, df_test = train_test_splitter(df, 'Generation')
+
+def label_delineator(df_train, df_test, label):
+    train_data = df_train.drop(label, axis=1).values
+    train_labels = df_train[label].values
+    test_data = df_test.drop(label, axis=1).values
+    test_labels = df_test[label].values
+    
+    return(train_data, train_labels, test_data, test_labels)
+
+train_data, train_labels, test_data, test_labels = label_delineator(df_train, df_test, 'isLegendary')
+
+def data_normalizer(train_data, test_data):
+    train_data = preprocessing.MinMaxScaler().fit_transform(train_data)
+    test_data = preprocessing.MinMaxScaler().fit_transform(test_data)
+    
+    return(train_data, test_data)
+
+train_data, test_data = data_normalizer(train_data, test_data)
+
+
+
+
+
+
+
